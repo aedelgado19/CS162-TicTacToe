13389b04d257e9ba686f1511d07175169caaeea2
aedelgado19 <aedelgado19@gmail.com>
2020-09-22 17:13:49 -0700

    added check win and reset board

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -90,6 +90,96 @@ int main() {
     }
   }// end while loop
 
+  //winning statements
+  char playAgain = 'y';
+
+  //if x, win = 1. if O, win = 2. If nobody wins or a tie, win = 0
+  if (checkWin() == 0){ //game tie
+    cout << "Tie!" << endl;
+  }
+  else if (checkWin() == 1){ //x win
+    cout << "X wins!" << endl;
+    xWins += 1; //increment num of wins
+    cout << "Amount of X wins: " << xWins << endl;
+    cout << "Amount of O wins: " << oWins << endl;
+  }
+  else if (checkWin() == 2){ //o win
+    cout << "O wins!" << endl;
+    oWins += 1; //increment num of wins
+    cout << "Amount of X wins: " << xWins << endl;
+    cout << "Amount of O wins: " << oWins << endl;
+  }
+
+  //play again?
+  cout << "Do you want to play again? (Y/N)" << endl;
+  cin << playAgain;
+  if (playAgain == 'Y' || playAgain == 'y'){
+    stillPlaying = true;
+  }
+  if (playAgain == 'N' || playAgain == 'n'){
+    stillPlaying = false;
+  }
+  //ALLY YOU MIGHT BE MISSING A CLOSING BRACKET HEREEEEEEEEEEEEE
   
   return 0;
+}
+
+//set up board
+void printBoard(){
+  cout << " \t1\t2\t3" << endl;
+
+  //print row
+  for (int r = 0; r < strlen(board); r++){ //ALLY THIS MAY NOT BE RIGHT!!!
+    //COME BACK AND DO THISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+  }
+
+
+}
+
+//set all spaces to blank
+void resetBoard(){
+  board[0][0] = BLANK;
+  board[1][0] = BLANK;
+  board[2][0] = BLANK;
+  board[0][1] = BLANK;
+  board[0][2] = BLANK;
+  board[1][1] = BLANK;
+  board[1][2] = BLANK;
+  board[2][1] = BLANK;
+  board[2][2] = BLANK;
+}
+
+/* Determine a win
+ * Algorithm: searches board for 3 moves in row,col,diagonal
+ * outputs: 0,1,2 where 0 = no win, 1 = X win, 2 = O win
+ */
+
+int checkWin(){
+  for (int i = 0; i <= 2; i++){
+    if(board[0][0] == i && board[0][1] == i && board[0][2] == i){
+      return i;
+    }
+    if(board[0][0] == i && board[1][1] == i && board[2][2] == i){
+      return i;
+    }
+    if(board[0][2] == i && board[1][1] == i && board[2][0] == i){
+      return i;
+    }
+    if(board[0][2] == i && board[1][2] == i && board[2][2] == i){
+      return i;
+    }
+    if(board[1][0] == i && board[1][1] == i && board[1][2] == i){
+      return i;
+    }
+    if(board[2][0] == i && board[2][1] == i && board[2][2] == i){
+      return i;
+    }
+    if(board[0][0] == i && board[1][0] == i && board[2][0] == i){
+      return i;
+    }
+    if(board[0][1] == i && board[1][1] == i && board[2][1] == i){
+      return i;
+    }
+  }
+ return 0; //nobody won
 }
